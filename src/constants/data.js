b6a95df06430787a4881bc3e5cf1460d6083308d
Hiep Bui <phuhiep11395@outlook.com>
2020-03-18 16:35:47 -0700

    Add endpoint for dev and prod

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,13 @@
 // export const API_BASE_URL = 'https://5dih1d57x5.execute-api.us-west-1.amazonaws.com/Prod';
-export const API_BASE_URL = 'https://7nfta4t8di.execute-api.us-west-1.amazonaws.com/Prod';
+const hostname = window && window.location && window.location.hostname;
+console.log(hostname);
+let endpoint = "";
+if(hostname === "dod-admin-20200226181705-hostingbucket-prod.s3-website-us-west-2.amazonaws.com") {
+    endpoint = 'https://7nfta4t8di.execute-api.us-west-1.amazonaws.com/Prod';
+} else {
+    endpoint = 'https://5dih1d57x5.execute-api.us-west-1.amazonaws.com/Prod';
+}
+export const API_BASE_URL = endpoint;
 export const ACCESS_TOKEN = 'accessToken';
 export const EMAIL = 'email';
 
